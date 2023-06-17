bbf5ff64027c124f3c55a4219dba1c3520c31702
João Lucas Pereira <j.lucaspereira@usp.br>
2023-06-17 00:41:31 -0300

    completed partial overview screen

--- proj_labbd/__main__.py
+++ proj_labbd/__main__.py
@@ -1,8 +1,6 @@
 from proj_labbd import app
 
 def main():
-    app.jinja_env.auto_reload = True
-    app.config['TEMPLATES_AUTO_RELOAD'] = True
     app.run(debug=True)
 
 if __name__ == "__main__":
