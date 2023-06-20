375871787f43962e7d7547ef0e8db19ddf0ccaac
Ryan S <ryansouza@usp.br>
2023-06-20 14:05:54 -0300

    relatorio Admin - incompleto (falta indice)

---
+++ database/6-reports-functions.sql
@@ -0,0 +1,68 @@
+BEGIN;
+
+-- ADMIN
+
+-- Relatório 1
+DROP FUNCTION IF EXISTS report1();
+CREATE OR REPLACE FUNCTION report1()
+    RETURNS TABLE
+            (
+                status text,
+                count  bigint
+            )
+AS
+$$
+BEGIN
+    RETURN QUERY
+        SELECT s.status, COUNT(*)
+        FROM results r
+                 JOIN status s USING (statusid)
+        GROUP BY s.status, s.statusid
+        ORDER BY s.statusid;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Relatório 2 -- Falta definir o Index (não analisei onde é melhor colocar)
+
+CREATE EXTENSION IF NOT EXISTS Cube;
+CREATE EXTENSION IF NOT EXISTS EarthDistance;
+
+DROP FUNCTION IF EXISTS report2(text);
+CREATE OR REPLACE FUNCTION report2(text)
+    RETURNS TABLE
+            (
+                city         text,
+                airport_iata char(3),
+                airport_name text,
+                airport_city text,
+                distance     double precision,
+                airport_type char(15)
+            )
+AS
+$$
+BEGIN
+    RETURN QUERY
+        SELECT a.city,
+               a.iatacode,
+               a.name,
+               a.city,
+               EARTH_DISTANCE(LL_TO_EARTH(a.latdeg, a.longdeg),
+                              LL_TO_EARTH(g15k.lat, g15k.long)) AS distance,
+               a.type
+        FROM airports a
+                 JOIN geocities15k g15k on g15k.name = $1
+        WHERE a.type IN ('medium_airport', 'large_airport')
+          AND a.isocountry = 'BR'
+          AND EARTH_DISTANCE(LL_TO_EARTH(a.latdeg, a.longdeg),
+                             LL_TO_EARTH(g15k.lat, g15k.long)) <= 100000
+        ORDER BY a.city, distance;
+END;
+$$ LANGUAGE plpgsql;
+
+-- Eu acho q esta certo mas n sei alguma cidade Brasileira para testar
+select *
+from report2('Boa Viagem');
+
+-- ESCUDEIRA
+
+COMMIT;
